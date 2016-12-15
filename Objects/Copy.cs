aa0faeccf6d0e5fd0ae04f06f44b0126b95b4e7e
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2016-12-15 11:40:41 -0800

    add copies table, Copy object and test. Trying to save copies

---
+++ Objects/Copy.cs
@@ -0,0 +1,270 @@
+using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Catalog.Objects
+{
+    public class Copy
+    {
+        public int Id { get; set; }
+        public int Quantity {get; set;}
+
+        public Copy(int quantity = 0, int copyId = 0)
+        {
+            this.Id = copyId;
+            this.Quantity = quantity;
+        }
+
+        public override bool Equals(System.Object otherCopy)
+        {
+            if (!(otherCopy is Copy))
+            {
+                return false;
+            }
+            else
+            {
+                Copy newCopy = (Copy)otherCopy;
+                bool checkId = (this.Id == newCopy.Id);
+                bool checkQuantity = (this.Quantity == newCopy.Quantity);
+                return (checkId && checkQuantity);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Quantity.GetHashCode();
+        }
+
+        public static List<Copy> GetAll()
+        {
+            List<Copy> allCopies = new List<Copy> { };
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM copies;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                int copyId = rdr.GetInt32(0);
+                int copyQuantity = rdr.GetInt32(1);
+                Copy newCopy = new Copy(copyQuantity, copyId);
+                allCopies.Add(newCopy);
+            }
+
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+
+            return allCopies;
+        }
+
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO copies (quantity) OUTPUT INSERTED.id VALUES(@CopyQuantity)", conn);
+
+            SqlParameter quantityParam = new SqlParameter("@CopyQuantity", this.Quantity);
+            cmd.Parameters.Add(quantityParam);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                this.Id = rdr.GetInt32(0);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+        public void AddCopy(Book newBook)
+        {
+          SqlConnection conn = DB.Connection();
+          conn.Open();
+          SqlCommand cmd = new SqlCommand("INSERT INTO copies (book_id, quantity) VALUES (@BookId, @CopyQuantity);", conn);
+          SqlParameter bookParam = new SqlParameter("@BookId", newBook.Id);
+          SqlParameter copyQuantityParam = new SqlParameter("@CopyQuantity", this.Quantity);
+          cmd.Parameters.Add(bookParam);
+          cmd.Parameters.Add(copyQuantityParam);
+          cmd.ExecuteNonQuery();
+
+          if (conn != null)
+          {
+            conn.Close();
+          }
+        }
+
+        // public static List<Book> SearchByTitle(string bookTitle)
+        // {
+        //   SqlConnection conn = DB.Connection();
+        //   conn.Open();
+        //
+        //   SqlCommand cmd = new SqlCommand("SELECT * FROM books WHERE title = (@BookTitle);", conn);
+        //
+        //   SqlParameter titleParameter = new SqlParameter("@BookTitle", bookTitle);
+        //   cmd.Parameters.Add(titleParameter);
+        //
+        //   SqlDataReader rdr = cmd.ExecuteReader();
+        //   List<Book> searchBook = new List<Book>{};
+        //   while (rdr.Read())
+        //   {
+        //     int searchId = rdr.GetInt32(0);
+        //     string searchTitle = rdr.GetString(1);
+        //     string searchDescription = rdr.GetString(2);
+        //     Book bookSearch = new Book(searchTitle, searchDescription, searchId);
+        //     searchBook.Add(bookSearch);
+        //   }
+        //   if (rdr != null)
+        //   {
+        //     rdr.Close();
+        //   }
+        //   if (conn != null)
+        //   {
+        //     conn.Close();
+        //   }
+        //   return searchBook;
+        // }
+        //
+        // public static List<Book> SearchByAuthor(string author)
+        // {
+        //   SqlConnection conn = DB.Connection();
+        //   conn.Open();
+        //
+        //   SqlCommand cmd = new SqlCommand((@"SELECT books.id, title, description FROM
+        //                                       authors_books JOIN books ON (authors_books.book_id = books.id)
+        //                                                     JOIN authors ON (authors.id = authors_books.author_id)
+        //                                                     WHERE authors.name = @AuthorName;"), conn);
+        //   SqlParameter authorParam = new SqlParameter("@AuthorName", author);
+        //   cmd.Parameters.Add(authorParam);
+        //
+        //   SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //   List<Book> bookResults = new List<Book>();
+        //   while (rdr.Read())
+        //   {
+        //     int id = rdr.GetInt32(0);
+        //     string title = rdr.GetString(1);
+        //     string desc = rdr.GetString(2);
+        //     Book book = new Book(title, desc, id);
+        //     bookResults.Add(book);
+        //   }
+        //   if (rdr != null)
+        //   {
+        //     rdr.Close();
+        //   }
+        //   if (conn != null)
+        //   {
+        //     rdr.Close();
+        //   }
+        //   return bookResults;
+        // }
+        //
+        // public static Book Find(int id)
+        // {
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("SELECT * FROM books WHERE id = (@BookId);", conn);
+        //
+        //     SqlParameter idParam = new SqlParameter("@BookId", id.ToString());
+        //     cmd.Parameters.Add(idParam);
+        //
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     int foundBookId = 0;
+        //     string foundBookTitle = null;
+        //     string foundBookDesc = "";
+        //
+        //     while (rdr.Read())
+        //     {
+        //         foundBookId = rdr.GetInt32(0);
+        //         foundBookTitle = rdr.GetString(1);
+        //         foundBookDesc = rdr.GetString(2);
+        //     }
+        //     Book newBook = new Book(foundBookTitle, foundBookDesc, foundBookId);
+        //
+        //     if (rdr != null)
+        //     {
+        //         rdr.Close();
+        //     }
+        //     if (conn != null)
+        //     {
+        //         conn.Close();
+        //     }
+        //
+        //     return newBook;
+        // }
+        //
+        // public void Update(string newTitle, string newDescription)
+        // {
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("UPDATE books SET title = @newTitle, description = @newDescription OUTPUT INSERTED.title, INSERTED.description WHERE id = @BookId;", conn);
+        //
+        //     SqlParameter newTitleParam = new SqlParameter("@newTitle", newTitle);
+        //     SqlParameter newDescParam = new SqlParameter("@newDescription", newDescription);
+        //     SqlParameter idParam = new SqlParameter("@BookId", this.Id);
+        //
+        //     cmd.Parameters.Add(newTitleParam);
+        //     cmd.Parameters.Add(newDescParam);
+        //     cmd.Parameters.Add(idParam);
+        //
+        //     SqlDataReader rdr = cmd.ExecuteReader();
+        //
+        //     while (rdr.Read())
+        //     {
+        //         this.Title = rdr.GetString(0);
+        //         this.Description = rdr.GetString(1);
+        //     }
+        //     if (rdr != null)
+        //     {
+        //         rdr.Close();
+        //     }
+        //     if (conn != null)
+        //     {
+        //         conn.Close();
+        //     }
+        // }
+
+        // public void Delete()
+        // {
+        //     SqlConnection conn = DB.Connection();
+        //     conn.Open();
+        //
+        //     SqlCommand cmd = new SqlCommand("DELETE FROM copies WHERE id = @CopyId", conn);
+        //     SqlParameter idParam = new SqlParameter("@CopyId", this.Id);
+        //     cmd.Parameters.Add(idParam);
+        //
+        //     cmd.ExecuteNonQuery();
+        //
+        //     if (conn != null)
+        //     {
+        //         conn.Close();
+        //     }
+        // }
+
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM copies;", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ Tests/CopyTest.cs
@@ -0,0 +1,153 @@
+using Xunit;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using Catalog.Startup;
+using Catalog.Objects;
+
+namespace Catalog.Tests
+{
+  public class CopyTests : IDisposable
+  {
+    public void BookTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=catalog_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DBReturnsEmptyAtFirst()
+    {
+      Assert.Equal(0, Copy.GetAll().Count);
+    }
+
+    [Fact]
+    public void Test_OverloadedEquals()
+    {
+      Copy copy1 = new Copy(1, 5);
+      Copy copy2 = new Copy(1, 5);
+      Assert.Equal(copy1, copy2);
+    }
+
+    [Fact]
+    public void Test_Save_SavesCopyToDatabase()
+    {
+      Copy testCopy = new Copy(1);
+      testCopy.Save();
+      Book newBook = new Book("The Sun Also Rises", "1930s Spain");
+      newBook.Save();
+      testCopy.AddCopy(newBook);
+
+      List<Copy> result = Copy.GetAll();
+      List<Copy> testList = new List<Copy>{testCopy};
+
+      Assert.Equal(testList, result);
+    }
+    //
+    // [Fact]
+    // public void Test_FindBookInDatabase()
+    // {
+    //   Book newBook = new Book("War and Peace", "lorem ipsum");
+    //   newBook.Save();
+    //
+    //   Book foundBook = Book.Find(newBook.Id);
+    //
+    //   Assert.Equal(newBook, foundBook);
+    // }
+    //
+    // [Fact]
+    // public void Test_GetAllBooksInLibrary_true()
+    // {
+    //   Book testBook1 = new Book("The Sun Also Rises", "Lorem ipsum");
+    //   Book testBook2 = new Book("The Grapes of Wrath", "Lorem ipsum");
+    //   testBook1.Save();
+    //   testBook2.Save();
+    //
+    //   List<Book> result = Book.GetAll();
+    //   List<Book> allBooks = new List<Book>{testBook1, testBook2};
+    //
+    //   Assert.Equal(result, allBooks);
+    // }
+    //
+    // [Fact]
+    // public void Test_UpdateBookInDatabase()
+    // {
+    //   string testName = "The Greats of Wrath";
+    //   Book testBook = new Book(testName, "Lorem ipsuM");
+    //   testBook.Save();
+    //   string newName = "The Grapes of Wrath";
+    //
+    //   testBook.Update(newName, "Lorem ipsum");
+    //   string result = testBook.Title;
+    //
+    //   Assert.Equal(newName, result);
+    // }
+    //
+    // [Fact]
+    // public void Test_Search_RetrievesListOfMatches()
+    // {
+    //   Book testBook1 = new Book("The Call of Cthulhu", "A story written by HP Lovecraft.");
+    //   Book testBook2 = new Book("Captain Underpants", "A silly children's book series.");
+    //   Book testBook3 = new Book("The Call of Cthulhu", "A second edition printing by Penguin Books.");
+    //   testBook1.Save();
+    //   testBook2.Save();
+    //   testBook3.Save();
+    //
+    //   List<Book> results = Book.SearchByTitle("The Call of Cthulhu");
+    //   List<Book> testAgainst = new List<Book>{testBook1, testBook3};
+    //
+    //   Assert.Equal(results, testAgainst);
+    // }
+    //
+    // [Fact]
+    // public void Test_Search_RetrievesBooksByAuthor()
+    // {
+    //   Book testBook1 = new Book("The Call of Cthulhu", "A story written by HP Lovecraft.");
+    //   Book testBook2 = new Book("Captain Underpants", "A silly children's book series.");
+    //   Book testBook3 = new Book("The Call of Cthulhu", "A second edition printing by Penguin Books.");
+    //   testBook1.Save();
+    //   testBook2.Save();
+    //   testBook3.Save();
+    //
+    //   Author newAuthor1 = new Author("HP Lovecraft");
+    //   Author newAuthor2 = new Author("Dav Pilkey");
+    //   newAuthor1.Save();
+    //   newAuthor2.Save();
+    //
+    //   testBook1.AddBook(newAuthor1);
+    //   testBook2.AddBook(newAuthor2);
+    //   testBook3.AddBook(newAuthor1);
+    //
+    //   List<Book> testList = new List<Book>{testBook1, testBook3};
+    //   List<Book> returnedList = Book.SearchByAuthor("HP Lovecraft");
+    //
+    //   Assert.Equal(testList, returnedList);
+    // }
+    //
+    // [Fact]
+    // public void Test_MultAuthors_AddsMultAuthorsToBook()
+    // {
+    //   Book testBook1 = new Book("The Call of Hamlet", "From the creators of Much Ado About Cthulhu.");
+    //   testBook1.Save();
+    //
+    //   Author newAuthor1 = new Author("HP Lovecraft");
+    //   Author newAuthor2 = new Author("William Shakespeare");
+    //   newAuthor1.Save();
+    //   newAuthor2.Save();
+    //
+    //   testBook1.AddBook(newAuthor1);
+    //   testBook1.AddBook(newAuthor2);
+    //   List<Book> returnedBook1 = Book.SearchByAuthor("HP Lovecraft");
+    //   List<Book> returnedBook2 = Book.SearchByAuthor("William Shakespeare");
+    //
+    //   Assert.Equal(returnedBook1, returnedBook2);
+    // }
+
+    public void Dispose()
+    {
+      Book.DeleteAll();
+      Author.DeleteAll();
+      Book.DeleteAllInJoinTable();
+      Copy.DeleteAll();
+    }
+  }
+}
